cbc3b581a5399b10043532095f1d2de611f2149f
Pavel Kornilov <lusever@lusever.com>
2014-04-04 17:18:33 +0400

    add main file. v.0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-CodeMirror-states
+CodeMirror States
 =================
 
-Export and import CodeMirror line classes, line widgets and markers.
+Export and import CodeMirror line classes, line widgets and markers. For example, to store on the server.
+
+## Usage
+
+You can get it from Bower:
+
+```sh
+bower install codemirror-states
+````
+
+Export:
+```javascript
+var states = editor.getStates()
+```
+
+Import:
+```javascript
+editor.setStates(states);
+```
+
+# License
+
+  MIT
---
+++ states.js
@@ -0,0 +1,132 @@
+/**
+ * Export and apply states from object.
+ * Support IE9+.
+ */
+(function() {
+  'use strict';
+
+  function objectFilter(obj, filterProps) {
+    var result = {};
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key) && filterProps[key]) {
+        result[key] = obj[key];
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * Get states into object.
+   * For export html from line widgets use option:
+   *   `getStateHTML: function`
+   * @param {{?markers: Array, ?lineWidgets: Array}} extraProp
+   * @return {{markers: Object, lineClasses: Array, lineWidgets: Array}}
+   */
+  CodeMirror.defineExtension('getStates', function(extraProp) {
+    var cm = this;
+    // list options from http://codemirror.net/doc/manual.html#markText
+    var markerOptionsArray = [
+      'className',
+      'inclusiveLeft',
+      'inclusiveRight',
+      'atomic',
+      'collapsed',
+      'clearOnEnter',
+      'clearWhenEmpty',
+      'replacedWith',
+      'handleMouseEvents',
+      'readOnly',
+      'addToHistory',
+      'startStyle',
+      'endStyle',
+      'title',
+      'shared'
+    ];
+
+    if (extraProp.markers) {
+      //concat and modify
+      lineWidgetOptionsArray.push.apply(context, [arguments])(lineWidgetOptionsArray, extraProp.markers);
+    }
+
+    var markerOptions = markerOptionsArray.reduce(function(prev, cur) {
+      prev[cur] = true;
+      return prev;
+    }, {});
+
+    var markers = cm.getAllMarks().map(function(marker) {
+      var data = marker.find(); // => {from, to}
+      data.options = objectFilter(marker, markerOptions);
+      return data;
+    });
+
+    var lineClasses = [];
+    var lineWidgets = [];
+
+    var lineWidgetOptionsArray = [
+      'coverGutter',
+      'noHScroll',
+      'above',
+      'showIfHidden',
+      'handleMouseEvents',
+      'insertAt'
+    ];
+
+    if (extraProp.lineWidgets) {
+      //concat and modify
+      lineWidgetOptionsArray.push.apply(lineWidgetOptionsArray, extraProp.lineWidgets);
+    }
+
+    var lineWidgetOptions = lineWidgetOptionsArray.reduce(function(prev, cur) {
+      prev[cur] = true;
+      return prev;
+    }, {});
+
+    cm.eachLine(function(lineHandle) {
+      if (lineHandle.widgets) {
+        lineWidgets.push(lineHandle.widgets.map(function(widget) {
+          return {
+            node: widget.getStateHTML ? widget.getStateHTML() : widget.node.outerHTML,
+            options: objectFilter(widget, lineWidgetOptions)
+          };
+        }));
+      } else {
+        lineWidgets.push(null);
+      }
+      lineClasses.push(lineHandle.textClass || null);
+    });
+
+    return {
+      lineClasses: lineClasses,
+      lineWidgets: lineWidgets,
+      markers: markers
+    };
+  });
+
+  /**
+   * Apply states.
+   * @param {{lineClasses: Object, lineWidgets: Object, markers: Object}} states
+   */
+  CodeMirror.defineExtension('setStates', function(states) {
+    var cm = this;
+
+    states.lineClasses.forEach(function(className, i) {
+      cm.addLineClass(i, 'text', className);
+    });
+
+    states.lineWidgets.forEach(function(widgets, lineNo) {
+      if (!widgets) {
+        return;
+      }
+      var div = document.createElement('div');
+      widgets.forEach(function(widget) {
+        div.innerHTML = widget.node;
+        cm.addLineWidget(lineNo, div.firstChild, widget.options);
+      });
+    });
+
+    states.markers.forEach(function(marker) {
+      cm.markText(marker.from, marker.to, marker.options);
+    });
+  });
+})();
