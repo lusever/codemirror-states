a39c1c8ddb23ccaac491cc07c720bca57dcab50c
Pavel Kornilov <lusever@lusever.com>
2015-01-21 17:50:36 +0300

    compability with CommonJS and AMD

--- states.js
+++ states.js
@@ -2,7 +2,15 @@
  * Export and apply states from object.
  * Support IE9+.
  */
-(function() {
+(function(mod) {
+  if (typeof exports == 'object' && typeof module == 'object') { // CommonJS
+    mod(require('../../lib/codemirror'));
+  } else if (typeof define == 'function' && define.amd) { // AMD
+    define(['../../lib/codemirror'], mod);
+  } else { // Plain browser env
+    mod(CodeMirror);
+  }
+})(function(CodeMirror) {
   'use strict';
 
   function objectFilter(obj, filterProps) {
